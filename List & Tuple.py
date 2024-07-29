5741d73381b910bcaeb80876666c6ee700501a54
Aman K <121393684+amankhandale@users.noreply.github.com>
2024-07-29 10:22:39 +0530

    Add files via upload

---
+++ List & Tuple.py
@@ -0,0 +1,59 @@
+
+# ! Lists (Creates Mutable sequence of value)
+# marks1 = 94.40
+# marks2 = 85.53
+# marks3 = 74.76
+# marks4 = 46.23
+
+# marks = [94.4, 85.53, 74.76]
+# print (marks)
+# print(type(marks))
+# print(marks[1])
+# print(len(marks))
+
+# ! Its mutable cuz any type of class can be stored in a list
+# student = ["Ark", 21 , "Pune"]
+# print(student)
+
+#! Slicing in List
+# marks = [85, 74, 23, 90, 30]
+# print(marks[1:3])
+# print(marks[-4:-1])
+
+# ~ List Methods
+# ^ List Append (Adds one element at the end)
+# list = [2, 1, 3]
+# list.append(4)
+# print(list)
+
+# ^ List Sort (Sorts in ascending order) [Reverse = Desending order]
+# list.sort(reverse=True)
+# print(list)
+
+# ^ List Reverse (Reverses the list)
+# list = ['a', 'b', 'c', 'd']
+# list.reverse()
+# print(list)
+
+# ^ List Insert (Insert element at index) {list.insert(index , element)}
+# list = [2, 1, 3, 4]
+# list.insert(1, 6)
+# print(list)
+
+# ^ List Remove (Removes first occurence of element)
+# list = [2, 1, 3, 1]
+# list.remove(1)
+# print(list)
+
+# ^ List Pop (Removes element at index)
+# list = [2, 1, 3, 1]
+# list.pop(2)
+# print(list)
+
+# ! Tuples (Creates Immmutable sequences of values)
+
+tuple = (1, 2 ,3 ,4)
+print(tuple[0])
+
+tup = ()
+print(type(tup))
