ef82f20fc4fe88e8876833d8e7a808b580424b23
Aman K <121393684+amankhandale@users.noreply.github.com>
2024-07-30 09:54:56 +0530

    Upload

--- 3. List & Tuple.py
+++ 3. List & Tuple.py
@@ -52,8 +52,52 @@
 
 # ! Tuples (Creates Immmutable sequences of values)
 
-tuple = (1, 2 ,3 ,4)
-print(tuple[0])
+# tuple = (1, 2 ,3 ,4)
+# print(tuple[0])
 
-tup = ()
-print(type(tup))
+# tup = ()
+# print(type(tup))
+
+# ? Write a program to ask user to enter name of their 3 fav movies & store them in a list
+
+# movies = []
+# mov1 = input("Enter 1st movie")
+# mov2 = input("Enter 2nd movie")
+# mov3 = input("Enter 3rd movie")
+
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+
+# print(movies)
+
+# ? Write a program to check if a list contains a palindrome of elements
+
+# list1 = [1,2,3,2,1]
+# list2 = [1,2,3]
+
+# copy_list1 = list1.copy()
+# copy_list1.reverse()
+# copy_list2 = list2.copy()
+# copy_list2.reverse()
+
+# if(copy_list1 == list1):
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+# if(copy_list2 == list2):
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+# ? Write a program to count the number of students with the "A" grade in the following tuple
+
+# grade = ("C", "D", "A", "B", "B", "A")
+# print(grade.count("C"))
+
+# ? Store the above value in a list & store them from "A" to "D".
+
+# list = ["B", "C", "A", "D"]
+# list.sort()
+# print(list)
