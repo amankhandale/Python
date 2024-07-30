1307588a5e237c16d07d893cbafa14de12f39b9e
Aman K <121393684+amankhandale@users.noreply.github.com>
2024-07-30 10:41:21 +0530

    save

---
+++ 4. Dictionary & Sets.py
@@ -0,0 +1,16 @@
+
+# ! Dictionary (Store data in a key:value pair)
+
+dict = {
+    "Key": "Value",
+    "Subject" : ["python", "Java", "C"],
+    "Name": "Ark",
+    "Age": 21,
+    "Straight": True,
+    "Marks": 85.7,
+}
+
+dict["Name"] = "Aman"
+dict["Age"] = 20
+print(dict)
+print(type(dict)) 
